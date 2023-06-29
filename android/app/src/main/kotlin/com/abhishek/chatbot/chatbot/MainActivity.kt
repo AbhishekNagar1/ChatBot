13a79a3c322ed290b346f28d399179387bf96138
Abhishek <abhisheknagar679@gmail.com>
2023-06-29 17:47:28 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/abhishek/chatbot/chatbot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.abhishek.chatbot.chatbot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/secrets.dart
@@ -0,0 +1,2 @@
+// const openAIAPIKey = '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';
+const openAIAPIKey = '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';
